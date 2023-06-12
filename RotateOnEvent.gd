715ee0da0b9fc8675f99e97a3ab8eec139e0495f
Florian Sabani <fsabani00@gmail.com>
2023-06-12 20:33:09 +0200

    Add files via upload

---
+++ RotateOnEvent.gd
@@ -0,0 +1,28 @@
+extends CSGBox
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
+func _input(ev):
+	if(ev is InputEventMouseButton):
+		# rotate_z(deg2rad(90)) # twin
+		var tween = get_node("Tween")
+	
+		tween.interpolate_property(self, "rotation_degrees",rotation, Vector3(
+			rotation.x,rotation.y,rotation.z + 45
+		), 1, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+		tween.start()
+		
