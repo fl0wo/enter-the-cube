421f48fccc9daf9a80d78a06ca60cc4e7e1fc72d
Florian Sabani <fsabani00@gmail.com>
2023-06-17 18:57:48 +0200

    Add files via upload

--- FollowPlayerRotation.gd
+++ FollowPlayerRotation.gd
@@ -2,11 +2,11 @@ extends Spatial
 onready var map = get_node('/root/WorldModelGenerator')
 var face_cube_to_rotation = [
 	[-36, 45, 0], # 0 V
+	[36, 135, 180], # 1
 	[36, 45, 60], # 2 V
 	[36, 45, -60], # 3 v
 	[-36, 135, -120], # 4 v
-	[-36, 135, 120], # 5
-	[36, 135, 180], # 1
+	[-36, -45, 120], # 5
 ]
 
 func _ready():
@@ -24,7 +24,7 @@ func _process(delta):
 	face_cube_to_rotation[face_index];
 
 	# float lerp_angle(from: float, to: float, weight: float)
-	var new_rotation = Vector3(
+	rotation_degrees = Vector3(
 		rad2deg(lerp_angle(
 			deg2rad(rotation_degrees.x),
 			deg2rad(target_rotation.x),
@@ -41,10 +41,3 @@ func _process(delta):
 			0.1
 		))
 	)
-	rotation_degrees = new_rotation
-
-	#var velocity Vector3.FORWARD var angular_acceleration 10 var speed 0
-	#func _physics_process(delta): if (Input.ls_actton_pressed("ui_up') II Inpot.is_action_pressed("ot_left.9 II Inpot.is_action_pressed(•ot_rtght') II Inpot.is_oction_presseWot_down•)): speed 0.5 velocity - vector3(Inpot.get_oction_strengtt('ot_right") - Inpot.get_oction_strength("oi_left'). 0, Inpot.get_oction_strengtt('ot_down') - Inpot.get_oction_strength("otop")).normalized()
-	#else: speed - 0
-	#Olobel.text "rotation : • • String(red2deglatan2(-velocity.x, -velocity.z))) Oplayer/oesh.rototion.y lerp(Splayer/nesh.rotation.y, aton21-velatty.x, -velocity.z), delta angular_acceleratton) $ployer.nove_ond_sltde(velocity • speed, Vector3.UP)
-
--- Player.gd
+++ Player.gd
@@ -1,21 +1,10 @@
 extends Spatial
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-var direction = Vector3.ZERO
-const speed = 20
-
 func isColliding(): return false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(dt):
 	pass
 
